e14f53ca741267faac579f59c7a74a1fc8adcedc
Marin Takagi <song.of.life@iCloud.com>
2017-03-23 22:12:38 +0900

    remove migration VERSION=20170302133042(remove_column_picture)

--- db/migrate/20170302133042_change_body_and_remove_picture_to_contacts.rb
+++
@@ -1,6 +0,0 @@
-class ChangeBodyAndRemovePictureToContacts < ActiveRecord::Migration[5.0]
-  def change
-    change_column :contacts, :body, :text
-    remove_column :contacts, :picture, :string
-  end
-end
